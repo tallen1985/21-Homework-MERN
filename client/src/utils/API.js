ca0883ab62b02f404e86a054c11f432a2bd081b2
Troy Allen <jake.allen@me.com>
2022-02-22 19:48:58 -0500

    update api.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,11 @@
 import { gql } from "@apollo/client";
 
 // route to get logged in user's info (needs the token)
+
+export const getMe = () => {
+  return "";
+};
+
 export const GET_USERS = gql`
   query getUsers {
     getUsers {
